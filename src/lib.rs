f85f9fccacc4cd6c257a1d05119e788364671f3a
nazeh <ar.nazeh@gmail.com>
2024-02-18 22:01:22 +0300

    feat: re-export DhtSettings

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub mod rpc;
 pub use crate::common::*;
 pub use crate::error::Error;
 pub use crate::rpc::response::*;
-pub use dht::{Dht, Testnet};
+pub use dht::{Dht, DhtSettings, Testnet};
 
 // Alias Result to be the crate Result.
 pub type Result<T, E = Error> = core::result::Result<T, E>;
