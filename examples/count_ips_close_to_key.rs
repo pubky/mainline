676e99b997b11d7e77a802032521c09910eaf384
Severin Buhler <sevi_buehler@hotmail.com>
2024-11-15 14:26:14 +0100

    more cleanup

--- examples/count_ips_close_to_key.rs
+++ examples/count_ips_close_to_key.rs
@@ -1,22 +1,8 @@
 use std::{collections::HashSet, net::IpAddr};
-
-use mainline::{rpc::DEFAULT_BOOTSTRAP_NODES, Dht, Id, Node};
-use rand::{random, seq::SliceRandom, thread_rng};
+use mainline::{Dht, Id, Node};
 use tracing::Level;
 
 
-fn get_random_boostrap_nodes(all: &HashSet<String>) -> Vec<String> {
-    if all.len() < 4 {
-        let nodes = DEFAULT_BOOTSTRAP_NODES;
-        let nodes: Vec<String> = nodes.into_iter().map(|node| node.to_string()).collect();
-        return nodes;
-    }
-    let mut all: Vec<String> = all.clone().into_iter().collect();
-    let mut rng = thread_rng();
-    all.shuffle(&mut rng);
-    let slice: Vec<String> = all[..4].into_iter().map(|va| va.clone()).collect();
-    slice
-}
 
 fn get_random_boostrap_nodes2() -> Vec<String> {
     let mut dht = Dht::client().unwrap();
@@ -27,21 +13,31 @@ fn get_random_boostrap_nodes2() -> Vec<String> {
     slice
 }
 
+fn init_dht(use_random_boostrap_nodes: bool) -> Dht {
+    if use_random_boostrap_nodes {
+        let bootstrap = get_random_boostrap_nodes2();
+        return Dht::builder().bootstrap(&bootstrap).build().unwrap();
+    } else {
+        Dht::client().unwrap()
+    }
+}
+
 
 fn main() {
     tracing_subscriber::fmt().with_max_level(Level::WARN).init();
 
     let k = 20; // Not really k but we take the k closest nodes into account.
+    const MAX_DISTANCE: u8 = 150; // Health check to not include outrageously distant nodes.
+    const USE_RANDOM_BOOTSTRAP_NODES: bool = true;
     let target = Id::random();
     let mut all_ips: HashSet<IpAddr> = HashSet::new();
-    let MAX_DISTANCE = 150;
 
-    println!("Count all IP addresses around a random target_key={target} k={k} max_distance={MAX_DISTANCE}.");
-
+    println!("Count all IP addresses around a random target_key={target} k={k} max_distance={MAX_DISTANCE} random_boostrap={USE_RANDOM_BOOTSTRAP_NODES}.");
+    println!();
+    
     let mut last_nodes: HashSet<IpAddr> = HashSet::new();
     for lookups in 1.. {
-        let bootstrap = get_random_boostrap_nodes2();
-        let mut dht = Dht::builder().bootstrap(&bootstrap).build().unwrap();
+        let mut dht = init_dht(USE_RANDOM_BOOTSTRAP_NODES);
         let nodes = dht.find_node(target).unwrap();
         let nodes: Vec<Node> = nodes.into_iter().filter(|node| target.distance(node.id()) < MAX_DISTANCE).collect();
         let closest_nodes: Vec<Node> = nodes.into_iter().take(k).collect();
